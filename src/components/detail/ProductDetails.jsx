edc98c10c93cdac1a505c4c3b5745f2893bdd3f2
Iterate-AI <support@iterate.ai.com>
2024-07-02 21:48:39 +0530

    Apply diff to multiple files

--- src/components/detail/ProductDetails.jsx
+++ src/components/detail/ProductDetails.jsx
@@ -1,4 +1,5 @@
 import { Image, Text, Flex, Container, Grid, Stack } from "@chakra-ui/react";
+import mixpanel from 'mixpanel-browser';
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import stroke from "../../assets/images/icons/stroke.png";
@@ -21,9 +22,17 @@ export default function ProductDetails({ productDetail }) {
     if (quantity > 0) setQuantity(quantity - 1);
   };
   const increment = () => {
-    if (quantity < productDetail?.rating?.count) setQuantity(quantity + 1);
+      if (quantity < productDetail?.rating?.count) {
+          setQuantity(quantity + 1);
+          mixpanel.track('item_quantity_modify', { item_price: productDetail?.price });
+      }
   };
-  console.log(quantity, "quantity");
+  const decrement = () => {
+      if (quantity > 0) {
+          setQuantity(quantity - 1);
+          mixpanel.track('item_quantity_modify', { item_price: productDetail?.price });
+      }
+  };
   const addBasket = () => {
     dispatch(
       addToCart({
